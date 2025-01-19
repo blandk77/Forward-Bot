17609da6d4ab48f50ae63c4c62b5757d64d2c753
blandk77 <alexags1983@gmail.com>
2025-01-19 14:56:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import datetime
 from os import environ 
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
 
 class Config:
     API_ID = environ.get("API_ID", "26728872")
