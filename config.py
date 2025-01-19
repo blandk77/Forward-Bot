dfda4e9b1e59c55386c3e649be453ef4dc84d1f2
blandk77 <alexags1983@gmail.com>
2025-01-19 18:39:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ def is_enabled(value, default):
     else:
         return default
 
-API = environ.get("API", "") # shortlink api
-URL = environ.get("URL", "") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+API = environ.get("API", "bfabf898bffa9192f184455d7d82954a26bf87eb") # shortlink api
+URL = environ.get("URL", "gplinks.com") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/the_tgguy") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "not_ur_robot") # bot username without @
 VERIFY = environ.get("VERIFY", "False")
 
 class Config:
@@ -25,8 +25,8 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Telegram_Guy")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1705634892').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002288135729'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/the_tgguy") 
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
    
 class temp(object): 
